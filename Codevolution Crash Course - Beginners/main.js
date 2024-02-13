45554ffae8d575aec4f2e6607cab85daaef0b200
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-02-13 16:12:18 +0100

    Start

---
+++ Codevolution Crash Course - Beginners/main.js
@@ -0,0 +1,79 @@
+// Log a statement using console.log()
+// console.log("Hello from main.js");
+
+// variables;
+// let age = 25;
+// console.log("Age is", age);
+
+// const salary = 80000;
+// console.log("Salary is", salary);
+
+// let sum = 0;
+// sum = 5;
+// console.log("Sum is", sum);
+
+// variables defined with "let" can be changed at a later time in the code
+// variables defines with "const" cannot and will return an error
+
+// const name = "Vishwas";
+// const language = "JavaScript";
+// const channel = `Codevolution`;
+
+// const total = 0;
+// const PI = 3.14;
+
+// const isPrimaryNumer = true;
+// const isNewUser = false;
+
+// let result;
+// console.log(result);
+
+// const res = undefined;
+
+// const data = null;
+
+// const person = {
+//   firstName: "Bruce",
+//   lastName: "Wayne",
+//   age: 30,
+// };
+
+// console.log(person.firstName);
+
+// const oddNumbers = [1, 3, 5, 7, 9];
+// console.log(oddNumbers[0]);
+// console.log(oddNumbers[1]);
+
+// let a = 10;
+// a = "Vishwas";
+// a = true;
+// console.log(a);
+
+// let x = 10;
+// let y = 5;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(++x);
+// console.log(--y);
+
+// console.log(x == y);
+// console.log(x !== y);
+// console.log(x > y);
+// console.log(x >= y);
+// console.log(x < y );
+// console.log(x <= y );
+
+// const isValidNumber = x > 20 || 8 > y;
+// console.log(isValidNumber);
+
+// const isValid = false;
+// console.log(!isValid);
+
+// console.log('Bruce ' + 'Wayne')
+
+const isEven = 10 % 2 === 0 ? 'Number is even' : 'Number is odd';
+console.log(isEven)
